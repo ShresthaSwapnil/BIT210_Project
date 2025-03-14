54750cedd4b9abc5809a07d9bd9adb7288d090b6
ayushbomjan2000@gmail.com <aryamanbista07@gmail.com>
2025-03-14 10:09:54 +0545

    Changed landing page

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,16 +1,14 @@
+// landing.component.ts
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './landing.component.html',
-  styleUrl: './landing.component.css'
+  styles: []
 })
 export class LandingComponent {
-  constructor(private router: Router) {}
-
-  explore() {
-    this.router.navigate(['/register']); // Redirects to the Register Page
-  }
+  constructor() {}
 }
